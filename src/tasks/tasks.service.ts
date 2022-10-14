02d58c8600ea20915999c7f809dda301c8909e0e
Mizanur Rahman <mizanur.rahman@navana.com>
2022-10-14 06:23:33 +0600

    Error handling with exception

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuidv4 } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -46,7 +46,13 @@ export class TasksService {
   }
 
   getTaskById(id: string): Task {
-    return this.tasks.find((task) => task.id === id);
+    const found = this.tasks.find((task) => task.id === id);
+
+    if (!found) {
+      throw new NotFoundException('Task with specified id not found!');
+    }
+
+    return found;
   }
 
   deleteTask(id: string): void {
